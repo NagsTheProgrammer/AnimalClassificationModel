35ef4ee622f5cb6c8e95329b28fc03d54198c4f9
Austyn Nagribianko <anagribianko@gmail.com>
2023-02-22 13:08:57 -0700

    FileConfig Commit
    pedestrian_detection_model.py
    pedestrian_detection_utils.py

--- PedestrianDetection.py
+++
@@ -1,9 +0,0 @@
-# 2023 - PedestrianDetectionModel - PedestrianDetection.py - Group 27
-
-import numpy as np
-
-def main():
-    print("Howdy")
-
-if __name__ == "__main__":
-    main()
---
+++ pedestrian_detection_utils.py
@@ -0,0 +1,9 @@
+# [YYYYMMDD] - [PROJECT NAME] - [FILENAME] - AUSTYN NAGRIBIANKO
+
+import numpy as np
+
+def main():
+    print("Suh")
+
+if __name__ == "__main__":
+    main()
